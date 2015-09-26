eb5229e52e541d07ca897654fb1b8954c358247b
wes calvert <wes.calvert@gmail.com>
2015-09-25 22:44:54 -0600

    Modified this when using lwip API

--- FRDM_K64F/EthernetInterface/EthernetInterface.h
+++ FRDM_K64F/EthernetInterface/EthernetInterface.h
@@ -20,7 +20,7 @@
 #ifndef ETHERNETINTERFACE_H_
 #define ETHERNETINTERFACE_H_
 
-#if !defined(TARGET_LPC1768) && !defined(TARGET_LPC4088) && !defined(TARGET_K64F)
+#if !defined(TARGET_LPC1768) && !defined(TARGET_LPC4088) && !defined(TARGET_LPC4088_DM) && !defined(TARGET_K64F) && !defined(TARGET_RZ_A1H) && !defined(TARGET_STM32F4)
 #error The Ethernet Interface library is not supported on this target
 #endif
 
@@ -80,11 +80,5 @@ public:
    */
   static char* getNetworkMask();
 };
-
-#include "TCPSocketConnection.h"
-#include "TCPSocketServer.h"
-
-#include "Endpoint.h"
-#include "UDPSocket.h"
 
 #endif /* ETHERNETINTERFACE_H_ */
